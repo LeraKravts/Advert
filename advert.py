45c2f5ee503a2c4690d2d00ee77c0e88439932a2
ValeriaKravtsova <lera.gor@inbox.ru>
2020-04-15 22:38:34 +0300

    add more files

---
+++ advert.py
@@ -0,0 +1,28 @@
+
+class ColorizeMixin:
+    def __init__(self, repr_color_code):
+        self.repr_color_code = repr_color_code
+
+
+class Advert(ColorizeMixin):
+
+    def __init__(self,  mapping, price=0):
+        self.price = price
+        self.repr_color_code = 33
+
+        for key in mapping:
+            if isinstance(mapping[key], dict):
+                value = Advert(mapping[key])
+            else:
+                value = mapping[key]
+            self.__setattr__(key, value)
+
+    def __setattr__(self, key, value):
+        if key == 'price' and value < 0:
+            raise Exception('ValueError: price must be >= 0')
+        if key == 'class':
+            key = 'class_'
+        self.__dict__[key] = value
+
+    def __repr__(self):
+        return f'\033[1;{self.repr_color_code};45m {self.title} | {self.price} ₽'
---
+++ main.py
@@ -0,0 +1,39 @@
+import json
+from advert import Advert
+from content import add1, add2
+
+
+def iphone_parsing(json_file):
+    mapping = json.loads(json_file)
+    iphone_ad = Advert(mapping)
+    return iphone_ad
+
+
+def corgi_parsing(json_file):
+    mapping = json.loads(json_file)
+    corgi = Advert(mapping)
+    return corgi
+
+
+def main():
+    iphone_ad = iphone_parsing(add1)
+    corgi = corgi_parsing(add2)
+    print(iphone_ad.location.address)
+    print(corgi.class_)
+    print(corgi)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
